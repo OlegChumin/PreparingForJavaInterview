541cfb78b4de59cc5015022d59a258b2270a190c
Oleg Chumin <tschumin.oleg@gmail.com>
2022-12-24 12:19:00 +0300

    Create new project

--- src/main/java/Comparable_and_Comparator/Comparable/SampleOfComparable.java
+++ src/main/java/Comparable_and_Comparator/Comparable/SampleOfComparable.java
@@ -1,9 +1,6 @@
 package Comparable_and_Comparator.Comparable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SampleOfComparable {
     public static void main(String[] args) {
@@ -13,11 +10,14 @@ public class SampleOfComparable {
         employeeList.add(new Employee(200, "Sandeep", "Pao", 2700));
         employeeList.add(new Employee(50, "Maria", "Munich", 2000));
         employeeList.add(new Employee(154, "Sunil", "Mumbay", 1000));
+        employeeList.add(new Employee(154, "Sunil", "Mumbay1", 2000));
+        employeeList.add(new Employee(154, "Sunil", "Mumbay2", 5000));
 
         System.out.println("Before sorting...");
         System.out.println(employeeList);
         System.out.println();
-        Collections.sort(employeeList);
+        Comparator salaryComparator = new SalaryComparator();
+        Collections.sort(employeeList, new NameComparator().thenComparing(new SurNameComparator().thenComparing(Employee::compareTo)));
         System.out.println("After sorting...");
         System.out.println(employeeList);
 
@@ -54,10 +54,44 @@ class Employee implements Comparable<Employee> {
 
     @Override
     public int compareTo(Employee anotherEmployee) {
-        if (this.id == anotherEmployee.id) {
-            return 0;
-        } else if (this.id < anotherEmployee.id) {
-            return -1;
-        } else return 1;
+//        if (this.id == anotherEmployee.id) {
+//            return 0;
+//        } else if (this.id < anotherEmployee.id) {
+//            return -1;
+//        } else return 1;
+        return this.id - anotherEmployee.id;
+    }
+
+
+//    @Override
+//    public int compareTo(Employee anotherEmployee) {
+//        return this.name.compareTo(anotherEmployee.name);
+//    }
+}
+
+class SalaryComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+//        return o1.salary.compareTo(o2.salary);  //change int salary to Integer salary!
+        return Integer.valueOf(o1.salary).compareTo(Integer.valueOf(o2.salary));
+    }
+}
+
+class NameComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+//        return o1.salary.compareTo(o2.salary);  //change int salary to Integer salary!
+        return o1.name.compareTo(o2.name);
+    }
+}
+
+class SurNameComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+//        return o1.salary.compareTo(o2.salary);  //change int salary to Integer salary!
+        return o1.surName.compareTo(o2.surName);
     }
 }
