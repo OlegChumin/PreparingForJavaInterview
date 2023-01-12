d19b4f8cbede210807fd23523d95b2bb55471c62
Oleg Chumin <tschumin.oleg@gmail.com>
2023-01-12 21:06:39 +0300

    Ad new files

---
+++ src/main/java/Streams/Part_1_map/StreamMethodMap.java
@@ -0,0 +1,56 @@
+package Streams.Part_1_map;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamMethodMap {
+    public static void main(String[] args) {
+        List<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("Hallo");
+        stringArrayList.add(" Leo!");
+        stringArrayList.add(" Be ");
+        stringArrayList.add("ready for ");
+        stringArrayList.add(" Java ");
+        stringArrayList.add(" interview!");
+
+        List<String> stringArrayList2 = new ArrayList<>(stringArrayList);
+
+        System.out.println("stringArrayList " + stringArrayList);
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            stringArrayList.set(i, String.valueOf(stringArrayList.get(i).length()));
+        }
+        System.out.println("stringArrayList after set method " + stringArrayList);
+        System.out.println();
+
+        List<Integer> integerList = stringArrayList2.stream().map(element -> element.length()).collect(Collectors.toList());
+        System.out.println("stringArrayList2 " + stringArrayList2);
+        System.out.println("integerList " + integerList);
+        System.out.println();
+
+        int[] array = {5, 9, 3, 8, 1};
+        System.out.println(Arrays.toString(array));
+        array = Arrays.stream(array).map(el -> {
+            if(el % 3 == 0) {
+                el = el / 3;
+            }
+            return el;
+        }).toArray();
+        System.out.println(Arrays.toString(array));
+        System.out.println();
+
+        Set<String> stringTreeSet = new TreeSet<>();
+        stringTreeSet.add("Hallo");
+        stringTreeSet.add(" Leo!");
+        stringTreeSet.add(" Be ");
+        stringTreeSet.add("ready for ");
+        stringTreeSet.add(" Java ");
+        stringTreeSet.add(" interview!");
+        stringTreeSet.add(" interview!");
+//        stringTreeSet.add(null);
+
+        System.out.println(stringTreeSet);
+        Set<Integer> integerSet = stringTreeSet.stream().map(el -> el.length()).collect(Collectors.toSet());
+        System.out.println(integerSet);
+        System.out.println();
+    }
+}
---
+++ src/main/java/Streams/Part_2_filter/StreamMethodFilter.java
@@ -0,0 +1,90 @@
+package Streams.Part_2_filter;
+
+import java.util.ArrayList;
+
+public class StreamMethodFilter {
+    public static void main(String[] args) {
+        StudentSample student1 = new StudentSample("Ivan", 'm', 22, 3, 8.3);
+        StudentSample student2 = new StudentSample("Nikolay", 'm', 28, 2, 6.4);
+        StudentSample student3 = new StudentSample("Elena", 'f', 19, 1, 8.9);
+        StudentSample student4 = new StudentSample("Petr", 'm', 35, 4, 7);
+        StudentSample student5 = new StudentSample("Maria", 'f', 23, 3, 9.1);
+        StudentSample student6 = new StudentSample("Oleg", 'm', 47, 3, 10.0);
+
+        ArrayList<StudentSample> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        students.add(student6);
+
+    }
+}
+
+class StudentSample {
+    private String name;
+    private char maleFemale;
+    private int age;
+    private int course;
+    private double averageGrade;
+
+    public StudentSample(String name, char maleFemale, int age, int course, double averageGrade) {
+        this.name = name;
+        this.maleFemale = maleFemale;
+        this.age = age;
+        this.course = course;
+        this.averageGrade = averageGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", maleFemale=" + maleFemale +
+                ", age=" + age +
+                ", course=" + course +
+                ", averageGrade=" + averageGrade +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getMaleFemale() {
+        return maleFemale;
+    }
+
+    public void setMaleFemale(char maleFemale) {
+        this.maleFemale = maleFemale;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+}
