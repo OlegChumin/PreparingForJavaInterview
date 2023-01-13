6b6ca01b6dcb021cb778ef22b5a50366d40b15c9
Oleg Chumin <tschumin.oleg@gmail.com>
2023-01-13 16:40:43 +0300

    Ad new files

--- src/main/java/Patterns/Pattern_05_Decorator/DecoratorPatternLesson.java
+++ src/main/java/Patterns/Pattern_05_Decorator/DecoratorPatternLesson.java
@@ -2,13 +2,68 @@ package Patterns.Pattern_05_Decorator;
 
 public class DecoratorPatternLesson {
     public static void main(String[] args) {
-
+        Pizza cheesePizza = new CheesePizza(new DoughPizza());
+        System.out.println(cheesePizza.makePizza());
+        Pizza cheesePepperoni = new PepperoniPizza(cheesePizza);
+        System.out.println(cheesePepperoni.makePizza());
+        Pizza meatPizzaCheesePepperoni = new MeatPizza(cheesePepperoni);
+        System.out.println(meatPizzaCheesePepperoni.makePizza());
+        Pizza pepperoniPizza =  new PepperoniPizza(new DoughPizza());
+        System.out.println(pepperoniPizza.makePizza());
     }
 }
 
 interface Pizza {
     String makePizza();
 }
+
+class DoughPizza implements Pizza {
+    @Override
+    public String makePizza() {
+        return "with ";
+    }
+}
+
+class CheesePizza implements Pizza {
+    Pizza pizza;
+
+    public CheesePizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    @Override
+    public String makePizza() {
+        return pizza.makePizza() + "cheese ";
+    }
+}
+
+class PepperoniPizza implements Pizza {
+    Pizza pizza;
+
+    public PepperoniPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    @Override
+    public String makePizza() {
+        return pizza.makePizza() + "pepperoni ";
+    }
+}
+
+class MeatPizza implements Pizza {
+    Pizza pizza;
+
+    public MeatPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    @Override
+    public String makePizza() {
+        return pizza.makePizza() + "meat ";
+    }
+}
+
+
 
 //class MakePizza implements Pizza{
 //    boolean isPepperoni;
