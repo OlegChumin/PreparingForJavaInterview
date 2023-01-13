34f2d963310ecadebba976197155da8518ccb23a
Oleg Chumin <tschumin.oleg@gmail.com>
2023-01-13 16:25:02 +0300

    Ad new files

--- src/main/java/Patterns/Pattern_05_Decorator/DecoratorPatternLesson.java
+++ src/main/java/Patterns/Pattern_05_Decorator/DecoratorPatternLesson.java
@@ -10,29 +10,41 @@ interface Pizza {
     String makePizza();
 }
 
-
-class CheesePizza implements Pizza{
-
-    @Override
-    public String makePizza() {
-        return "with cheese ";
-    }
-}
-
-class PepperoniPizza implements Pizza{
-
-    @Override
-    public String makePizza() {
-        return "with pepperoni ";
-    }
-}
-
-class CheesePepperoniiPizza implements Pizza{
-
-    @Override
-    public String makePizza() {
-        return "with cheese pepperoni ";
-    }
-}
+//class MakePizza implements Pizza{
+//    boolean isPepperoni;
+//    boolean isCheese;
+//    boolean isCheesePepperoni;
+//    // 25
+//
+//    @Override
+//    public String makePizza() {
+//        return "with " + (isPepperoni ? "pepperoni " : "") + (isCheese ? "cheese " : "" + (isCheesePepperoni ? "cheese pepperoni " : ""));
+//    }
+//}
+//
+//
+//class CheesePizza implements Pizza{
+//
+//    @Override
+//    public String makePizza() {
+//        return "with cheese ";
+//    }
+//}
+//
+//class PepperoniPizza implements Pizza{
+//
+//    @Override
+//    public String makePizza() {
+//        return "with pepperoni ";
+//    }
+//}
+//
+//class CheesePepperoniiPizza implements Pizza{
+//
+//    @Override
+//    public String makePizza() {
+//        return "with cheese pepperoni ";
+//    }
+//}
 
 
