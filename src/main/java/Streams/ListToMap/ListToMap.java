c068f4a9c3becf557c7d555634f1626c075afe41
Oleg Chumin <tschumin.oleg@gmail.com>
2023-01-21 12:25:34 +0300

    Ad new files

--- src/main/java/Streams/ListToMap/ListToMap.java
+++ src/main/java/Streams/ListToMap/ListToMap.java
@@ -10,15 +10,16 @@ public class ListToMap {
     public static void main(String[] args) {
         List<String> arrayList = getRandomStringList(); // creates random arrayList of Strings
         System.out.println(arrayList);
+
         Map<String, Integer> treeNewMap = getMapOfRepeatableItemsFromList(arrayList);
         System.out.println(treeNewMap);
     }
 
     static List<String> getRandomStringList() {
-        final int RANGE_OF_INT_FOR_STRING = 100;
+        final int RANGE_OF_INT_FOR_STRING = 10;
         List<String> list = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < RANGE_OF_INT_FOR_STRING / 10; i++) {
+        for (int i = 0; i < RANGE_OF_INT_FOR_STRING; i++) {
             list.add(((Integer) random.nextInt(RANGE_OF_INT_FOR_STRING)).toString());
         }
         return list;
@@ -26,18 +27,20 @@ public class ListToMap {
 
     @MethodDescription
     static Map<String, Integer> getMapOfRepeatableItemsFromList(List<String> stringList) {
+
         Map<String, Integer> treeMap = new TreeMap<>(); // for auto sorting during adding elements
+
         for (int i = 0; i < stringList.size(); i++) {
-            Integer entryOfElementsCount = 0; // counter of the equals element entry in list
+            int entryOfElementsCount = 0; // counter of the equals element entry in list
             String element = stringList.get(i); // get current element from list
             for (int j = 0; j < stringList.size(); j++) {
-                if(element.equals(stringList.get(i))) {
+                if (element.compareToIgnoreCase(stringList.get(i)) == 0) {
                     entryOfElementsCount++;
                 }
             }
             treeMap.put(element, entryOfElementsCount);
         }
-        return new TreeMap<>(); // return filled treeMap
+        return treeMap; // return filled treeMap
     }
 
 }
